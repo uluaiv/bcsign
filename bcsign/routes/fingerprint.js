0a94cb0de1ff1e6f300f2616a198719ca6fcd6cc
Valentin Djomin <Valentin Djomin>
2016-02-01 11:32:30 +0200

    Ajax request, save object to the memory storage

--- bcsign/routes/fingerprint.js
+++ bcsign/routes/fingerprint.js
@@ -2,9 +2,22 @@ var express = require('express');
 var multer = require('multer');
 var router = express.Router();
 
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage }).any();
+
 router.get('/', function(req, res, next) {
   res.render('fingerprint', { title: 'fingerprint' });
 });
 
+router.post('/',function(req,res){
+	upload(req,res,function(err) {
+                if(err) {
+                    return res.end("Error uploading file.");
+		}
+        //console.log(req.files[0].buffer);
+        res.end('Done!');
+	});
+        
+});
 
 module.exports = router;
