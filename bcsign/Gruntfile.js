47d713f8aae968c5897b300f52cdcb8387eba75e
ValentinD <valentin.djomin@ignite.ee>
2016-02-08 19:09:58 +0200

    Dependencies changed

--- bcsign/Gruntfile.js
+++ bcsign/Gruntfile.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -35,5 +35,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.registerTask('build', ['clean']);
-    grunt.registerTask('dist', ['sync', 'build', 'bower', 'uglify:legacy']);
+    grunt.registerTask('dist', ['sync', 'build', 'bower']);
 };
